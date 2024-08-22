4a6aa45a871a4aba4181908832c1a46ecf5be657
dnstkrv <denistokarev@mail.ru>
2024-08-22 09:25:08 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+opkg update &> /dev/null
+opkg install python3 python3-pip
+curl -O https://bootstrap.pypa.io/get-pip.py &> /dev/null
+python get-pip.py
+rm get-pip.py
+pip install pyTelegramBotAPI telethon pathlib
+
+mkdir -p "/opt/etc/telegram4kvas"
+
+curl -o /opt/etc/telegram4kvas/telegram_bot_config.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot_config.py
+curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot.py
+curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/S98telegram4kvas
+
+echo -e "\n\nВнесите API ключ, полученный от BotFather и Ваш логин Телеграм в файл \"/opt/etc/telegram4kvas/telegram_bot_config.py\"\n\nПосле чего выполните команду \"/opt/etc/init.d/S98telegram4kvas start\" для запуска бота\n\n"
---
+++ telegram_bot.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+import stat
+import tempfile
+import telebot
+from telebot import types
+from telethon.sync import TelegramClient
+import requests
+import telegram_bot_config as config
+
+token = config.token
+usernames = config.usernames
+bot = telebot.TeleBot(token)
+action = ""
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.from_user.username not in usernames:
+        bot.send_message(message.chat.id, 'Вы не авторизованы, внесите свой логин в список')
+        return
+    startMenu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Управление хостами")
+    item2 = types.KeyboardButton("Сервис")
+    startMenu.add(item1, item2)
+    bot.send_message(message.chat.id, 'Добро пожаловать в панель управления КВАС', reply_markup=startMenu)
+
+@bot.message_handler(content_types=['text'])
+def bot_message(message):
+    try:
+        mainMenu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        m1 = types.KeyboardButton("Управление хостами")
+        m2 = types.KeyboardButton("Сервис")
+        mainMenu.add(m1, m2)
+
+        serviceMenu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        s1 = types.KeyboardButton("Запустить debug")
+        s2 = types.KeyboardButton("Перезагрузить роутер")
+        backward = types.KeyboardButton("Назад")
+        serviceMenu.add(s1, s2)
+        serviceMenu.add(backward)
+        
+        hostsMenu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        h1 = types.KeyboardButton("Добавить хост")
+        h2 = types.KeyboardButton("Удалить хост")
+        h3 = types.KeyboardButton("Список хостов")
+        backward = types.KeyboardButton("Назад")
+        hostsMenu.add(h1, h2)
+        hostsMenu.add(h3, backward)
+        
+        if message.from_user.username not in usernames:
+            bot.send_message(message.chat.id, 'Вы не авторизованы, внесите свой логин в список')
+            return
+            
+        if message.chat.type == 'private':
+            global action
+            
+            if message.text == 'Управление хостами':
+                bot.send_message(message.chat.id, 'Управление хостами:', reply_markup=hostsMenu)
+                return
+                
+            if message.text == 'Сервис':
+                bot.send_message(message.chat.id, 'Сервисное меню', reply_markup=serviceMenu)
+                return
+               
+            if message.text == 'Добавить хост':
+                bot.send_message(message.chat.id, 'Введите домен:', reply_markup=hostsMenu)
+                action = "hostAdd"
+                return
+                
+            if action == "hostAdd":
+                bot.send_message(message.chat.id, 'Выполняется добавление адреса, ожидайте', reply_markup=serviceMenu)
+                with tempfile.TemporaryFile() as tempf:
+                    hostAdd = subprocess.Popen(['kvas', 'add', message.text, 'yes'], stdout=tempf)
+                    hostAdd.wait()
+                    tempf.seek(0)
+                    a = tempf.read().decode('utf-8')
+                    #Удаляем лишние символы и отправляем ответ
+                    bot.send_message(message.chat.id, a.replace('-', '').replace('[33m', '').replace('[m', '').replace('[10D', '').replace('[1;32m', ''), reply_markup=hostsMenu)
+                action = ""
+                return
+                
+            if message.text == 'Удалить хост':
+                bot.send_message(message.chat.id, 'Введите домен:', reply_markup=hostsMenu)
+                action = "hostDel"
+                return
+                
+            if action == "hostDel":
+                bot.send_message(message.chat.id, 'Выполняется удаление адреса, ожидайте', reply_markup=serviceMenu)
+                with tempfile.TemporaryFile() as tempf:
+                    hostDel = subprocess.Popen(['kvas', 'del', message.text], stdout=tempf)
+                    hostDel.wait()
+                    tempf.seek(0)
+                    a = tempf.read().decode('utf-8')
+                    #Удаляем лишние символы и отправляем ответ
+                    bot.send_message(message.chat.id, a.replace('-', '').replace('[33m', '').replace('[m', '').replace('[8D', '').replace('[1;32m', ''), reply_markup=hostsMenu)
+                action = ""
+                return
+              
+            if message.text == 'Список хостов':
+                file = open('/opt/etc/hosts.list')
+                flag = True
+                s = '```'
+                sites = []
+                for line in file:
+                    sites.append(line)
+                    flag = False
+                if flag:
+                    s = 'Список пуст'
+                file.close()
+                sites.sort()
+                if not flag:
+                    for line in sites:
+                        s = str(s) + '\n' + line.replace("\n", "")
+                s = str(s) + '\n```'
+                if len(s) > 4096:
+                    for x in range(0, len(s), 4096):
+                        bot.send_message(message.chat.id, s[x:x + 4096])
+                else:
+                    bot.send_message(message.chat.id, s, parse_mode="Markdown")
+                return
+
+            if message.text == 'Запустить debug':
+                bot.send_message(message.chat.id, 'Файл kvas.debug готовится к отправке', reply_markup=serviceMenu) 
+                debug = subprocess.Popen(["kvas", "debug", "kvas.debug"])
+                debug.wait()
+                bot.send_document(message.chat.id, open(r'/opt/root/kvas.debug', 'rb'), reply_markup=serviceMenu)
+
+            if message.text == "Назад":
+                bot.send_message(message.chat.id, 'Добро пожаловать в панель управления КВАС', reply_markup=mainMenu)
+                return
+                
+            if message.text == 'Перезагрузить роутер':
+                bot.send_message(message.chat.id, 'Отправлена команда для перезагрузки роутера, ожидайте', reply_markup=serviceMenu)
+                os.system('reboot')
+                return
+
+    except Exception as error:
+        file = open("/opt/error.log", "w")
+        file.write(str(error))
+        file.close()
+        os.chmod(r"/opt/error.log", 0o0755)
+
+try:
+    bot.infinity_polling()
+except Exception as err:
+    fl = open("/opt/error.log", "w")
+    fl.write(str(err))
+    fl.close()
