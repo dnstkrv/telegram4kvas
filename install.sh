56ff2c3a6e9d558ed5a21a8c0bf8f1b6cf6dd031
dnstkrv <denistokarev@mail.ru>
2024-08-22 16:02:31 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ pip install pyTelegramBotAPI pathlib
 
 mkdir -p "/opt/etc/telegram4kvas"
 
-curl -o /opt/etc/telegram4kvas/telegram_bot_config.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot_config.py
-curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot.py
-curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/S98telegram4kvas
+curl -o /opt/etc/telegram4kvas/telegram_bot_config.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev/telegram_bot_config.py
+curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev/telegram_bot.py
+curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev/S98telegram4kvas
 
 chmod +x /opt/etc/init.d/S98telegram4kvas
 
