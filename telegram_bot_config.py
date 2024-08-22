3277cad117fe4102aff08937cbe4b7401ab2e705
dnstkrv <denistokarev@mail.ru>
2024-08-22 15:31:35 +0300

    Update telegram_bot_config.py

--- telegram_bot_config.py
+++ telegram_bot_config.py
@@ -1,3 +1,3 @@
 token = 'insertAPI'  # API ключ бота, полученный от @BotFather
-usernames = ['insertLogin']  # Ваш логин в Телеграм
-#userid = ['insertId'] # Или Ваш ID, необязательно вводить и логин и ID, можно что-то одно
+usernames = ['insertLogin1', 'insertLogin2']  # Ваш логин(или несколько) в Телеграм
+userid = [insertId, insertid2] # Или Ваш ID(или несколько) без кавычек, необязательно вводить и логин и ID, можно что-то одно
