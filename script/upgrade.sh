3e8183edfa5796d01944ca703b3e6c7a9fe6a3d8
dnstkrv <denistokarev@mail.ru>
2024-08-28 22:04:33 +0300

    Update upgrade.sh

--- script/upgrade.sh
+++ script/upgrade.sh
@@ -5,8 +5,8 @@
 rm /opt/etc/telegram4kvas/telegram_bot.py
 rm /opt/etc/init.d/S98telegram4kvas
 
-curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev/telegram_bot.py
-curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev/S98telegram4kvas
+curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev1/telegram_bot.py
+curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/dev1/S98telegram4kvas
 
 chmod +x /opt/etc/init.d/S98telegram4kvas
 
