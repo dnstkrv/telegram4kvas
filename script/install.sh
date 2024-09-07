6491c5759e5d65a7bc793a27d3ba76cc7b1f57a2
dnstkrv <denistokarev@mail.ru>
2024-09-07 12:44:16 +0300

    Download path changed

--- script/install.sh
+++ script/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+bot_path="/opt/etc/telegram4kvas/telegram_bot.py"
 config_path="/opt/etc/telegram4kvas/telegram_bot_config.py"
 release_url=https://api.github.com/repos/dnstkrv/telegram4kvas/releases
+package_url=$(curl -sH "Accept: application/vnd.github.v3+json" ${release_url}/latest | sed -n 's/.*browser_download_url\": "\(.*\)\"/\1/p;'| tr -d ' ' |  sed '/^$/d')
 latest_version=$(curl -sH "Accept: application/vnd.github.v3+json" ${release_url}/latest | grep tag_name | awk -F\" '{print $4}')
 
 if [ "$1" = "-remove" ]; then
@@ -41,11 +43,11 @@ if [ "$1" = "-install" ]; then
     mkdir -p "/opt/etc/telegram4kvas"
     
     curl -o /opt/etc/telegram4kvas/telegram_bot_config.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot_config.py
-    curl -o /opt/etc/telegram4kvas/telegram_bot.py https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/telegram_bot.py
     curl -o /opt/etc/init.d/S98telegram4kvas https://raw.githubusercontent.com/dnstkrv/telegram4kvas/main/S98telegram4kvas
-    
     chmod +x /opt/etc/init.d/S98telegram4kvas
     
+    curl -JLo $bot_path $package_url
+
     config_file="/opt/etc/telegram4kvas/telegram_bot_config.py"
     echo -e "\nВведите API ключ, полученный от BotFather:"
     read api
