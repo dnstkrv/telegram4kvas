716df8543369142c9c2db7e5182473996f1445e0
Денис Свириденко <d.sviridenko@aamsystems.ru>
2025-01-13 09:33:20 +0300

    Add connection attempts and timeout

--- telegram_bot.py
+++ telegram_bot.py
@@ -1013,7 +1013,15 @@ def go_back(message: types.Message):
 if __name__ == "__main__":
     try:
         bot.setup_middleware(Middleware())
-        bot_me = bot.get_me()
+        connection_attempt = 0
+        while connection_attempt != telegram_bot_config.reconnection_attempts:
+            connection_attempt +=1
+            try:     
+                bot_me = bot.get_me()
+                connection_attempt = telegram_bot_config.reconnection_attempts
+            except Exception as e: 
+                logger.warning(f'Connection fail. Wait {telegram_bot_config.reconnection_timeout} seconds to reconnect')
+                time.sleep(telegram_bot_config.reconnection_timeout)
         os.system(
             f"logger -s -t telegram4kvas Bot @{bot_me.username} version: {telegram_bot_config.version} running..."
         )
